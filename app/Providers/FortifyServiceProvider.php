869f2d1c71e48dcbcf2f112b5e7771d83196b0ee
alif <alifianfajarpratama@gmail.com>
2021-04-12 11:17:24 +0700

    Update Register Response

--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -6,11 +6,16 @@ use App\Actions\Fortify\CreateNewUser;
 use App\Actions\Fortify\ResetUserPassword;
 use App\Actions\Fortify\UpdateUserPassword;
 use App\Actions\Fortify\UpdateUserProfileInformation;
+use App\Models\User;
 use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\ServiceProvider;
 use Laravel\Fortify\Fortify;
+use Illuminate\Support\Facades\Hash;
+
+use Illuminate\Contracts\Auth\StatefulGuard;
+use Laravel\Fortify\Http\Responses\RegisterResponse as FortifyRegisterResponse;
 
 class FortifyServiceProvider extends ServiceProvider
 {
@@ -51,6 +56,21 @@ class FortifyServiceProvider extends ServiceProvider
 
         Fortify::loginView(function(){
             return view('auth/login');
+        });
+
+        $this->app->singleton(
+            \Laravel\Fortify\Contracts\RegisterResponse::class,
+            \App\Http\Responses\RegisterResponse::class,
+        );
+
+        Fortify::authenticateUsing(function (Request $request) {
+            $user = User::where('username', $request->username)->first();
+            
+            if ($user &&
+                Hash::check($request->password, $user->password) &&
+                $user->is_active == 1) {
+                return $user;
+            }
         });
     }
 }
